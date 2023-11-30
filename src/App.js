f243e326d9d3faf16aefd8049da6214fcf29f374
Heilika <143094865+Heilika@users.noreply.github.com>
2023-11-30 17:34:45 +0200

    up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
+
 import './App.css';
+
 import WeatherForecast from './WeatherForecast';
 
 function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [selectedLocation, setSelectedLocation] = useState(undefined);
   const [selectedLocationWeather, setSelectedLocationWeather] = useState(undefined);
-  const [locations, setLocations] = useState([
+  const [newLocationLatitude, setNewLocationLatitude] = useState('');
+  const [newLocationLongitude, setNewLocationLongitude] = useState('');
+  const [newLocationName, setNewLocationName] = useState('');
+  const [locations, setLocations] = useState ([
     {
       name: 'Tallinn',
       latitude: 59.4370,
@@ -22,54 +27,73 @@ function App() {
   const selectLocation = (location) => {
     setSelectedLocation(location);
     getLocationData(location);
-  };
+  }
 
   const getLocationData = async (location) => {
     setIsLoading(true);
-    const data = await fetch(
-      `https://api.open-meteo.com/v1/forecast?latitude=${location.latitude}&longitude=${location.longitude}&current=temperature_2m&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_probability_max,wind_speed_10m_max`
-    );
+    const data = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${location.latitude}&longitude=${location.longitude}&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum,wind_speed_10m_max&timezone=auto`);
     const dataJson = await data.json();
     setIsLoading(false);
-
-    console.log(dataJson);
     setSelectedLocationWeather(dataJson);
+  }
+  const addNewLocation = () => {
+    const newLocation = {
+      name: newLocationName,
+      latitude: parseFloat(newLocationLatitude),
+      longitude: parseFloat(newLocationLongitude),
+    };
+    setLocations([...locations, newLocation]);
+    setNewLocationName('');
+    setNewLocationLatitude('');
+    setNewLocationLongitude('');
+  }
+  const handleInputChange = (event) => {
+    setNewLocationName(event.target.value);
   };
 
   return (
     <div className="App">
       <header className="App-header" style={{ display: 'flex', flexDirection: 'row', gap: 24, padding: 24 }}>
         <div style={{ borderRight: '1px solid gray', padding: '0 24px 0 0' }}>
-          <h3>Asukohad</h3>
+          <h3>Locations</h3>
           {locations.map((location) => (
             <div key={location.name}>
-              <button onClick={() => selectLocation(location)}>{location.name}</button>
+              <a onClick={() => selectLocation(location)}>{location.name}</a>
             </div>
           ))}
+          <h3>New location</h3>
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <input
+            type='text'
+              placeholder='Name'
+              value={newLocationName}
+              onChange={handleInputChange}
+            />
+            <input
+              placeholder='Longitude'
+              value={newLocationLongitude}
+              onChange={(e) => setNewLocationLongitude(e.target.value)}
+            />
+            <input
+              placeholder='Latitude'
+              value={newLocationLatitude}
+              onChange={(e) => setNewLocationLatitude(e.target.value)}
+            />
+            <button onClick={addNewLocation}>Add location</button>
+          </div>
         </div>
         <div style={{ textAlign: 'left' }}>
           {selectedLocation ? (
             <>
-              <h3>Ilmateade</h3>
-              <div>
-                <div>{selectedLocation.name}</div>
-                {isLoading ? (
-                  'Laen...'
-                ) : selectedLocationWeather && selectedLocationWeather.current ? (
-                  <div>
-                    Asukohas {selectedLocation.name} on {selectedLocationWeather.current.temperature_2m}
-                    {selectedLocationWeather.current_units.temperature_2m}.
-                    <WeatherForecast selectedLocationWeather={selectedLocationWeather} />
-                  </div>
-                ) : (
-                  <div>Ei leitud ilmateavet valitud asukohale.</div>
-                )}
-              </div>
+              <h3>Weather forecast {selectedLocation.name}</h3>
+              {isLoading ?
+                'Laen...' :
+                <WeatherForecast weather={selectedLocationWeather} />}
             </>
           ) : (
             <>
-              <h3>Ilmateadet ei ole</h3>
-              <div>Ilmateate vaatamiseks vali linn!</div>
+              <h3>No weather forecast available</h3>
+              <div>Please select a location!</div>
             </>
           )}
         </div>
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,53 +1,49 @@
-import React from 'react';
+
 import piltSunrise from './sunrise.png';
 import piltSunset from './sunset.png';
 
-function WeatherForecast({ selectedLocationWeather }) {
-    const formatDate = (dateString) => {
-        const date = new Date(dateString);
-        const formattedDate = `${date.getDate()}.${date.getMonth() + 1}.`;
-        return formattedDate;
+
+
+const WeatherForecast = ({ weather }) => {
+    if (!weather || !weather.daily || !weather.daily.time) {
+        return <div>No weather data available</div>;
+    }
+    const convertKmphToMps = (speedKmph) => {
+        const speedMps = speedKmph * 1000 / (60 * 60);
+        return speedMps.toFixed(1);
     };
 
-if (selectedLocationWeather && selectedLocationWeather.daily && selectedLocationWeather.daily.length) {
-    const startDate = new Date('2023-11-24'); // Date object for November 24th
-    const endDate = new Date('2023-11-30'); // Date object for November 30th
+    const convertDateStringToHoursAndMinutes = (dateString) => {
+        return `${(new Date(dateString)).getHours()}.${(new Date(dateString)).getMinutes()}`;
+    };
 
-    const filteredData = selectedLocationWeather.daily.filter((day) => {
-      const dayDate = new Date(day.date);
-      return dayDate >= startDate && dayDate <= endDate;
-    });
-   
-    return (
-        <table>
-            <thead>
+
+    <table cellSpacing={24}>
+        <thead>
+            <tr>
+                <th>Prognoos</th>
+                <th><img style={{ width: '75px' }} src={piltSunrise} alt="Sunrise" /></th>
+                <th><img style={{ width: '75px' }} src={piltSunset} alt="Sunset" /></th>
+                <th>Max/Min °C</th>
+                <th>Sademed %</th>
+                <th>Tuul m/s</th>
+
+            </tr>
+        </thead>
+        <tbody>
+            {weather.daily.time.map((_, i) => (
                 <tr>
-                    <td>Prognoos</td>
-                    <td><img style={{ width: '75px' }} src={piltSunrise} alt="Sunrise" /></td>
-                    <td><img style={{ width: '75px' }} src={piltSunset} alt="Sunset" /></td>
-                    <td>Max/Min °C</td>
-                    <td>Sademed %</td>
-                    <td>Tuul m/s</td>
+                    <td>{weather.daily.time[i]}</td>
+                    <td>{convertDateStringToHoursAndMinutes(weather.daily.sunrise[i])}/{convertDateStringToHoursAndMinutes(weather.daily.sunset[i])}</td>
+                    <td>{weather.daily.temperature_2m_min[i]}/{weather.daily.temperature_2m_max[i]}</td>
+                    <td>{weather.daily.precipitation_sum[i]}mm</td>
+                    <td>{convertKmphToMps(weather.daily.wind_speed_10m_max[i])}m/s</td>
                 </tr>
-            </thead>
-            <tbody>
-                {filteredData.map((day, index) => (
-                    <tr key={index}>
-                        <td>{formatDate(day.date, 'dddd D. MMM')}</td>
-                        <td>{formatDate(day.sunrise, 'HH:mm')}</td>
-                        <td>{formatDate(day.sunset, 'HH:mm')}</td>
-                        <td align="right">{day.temperature_2m_max} / {day.temperature_2m_min}</td>
-                        <td align="right">{day.precipitation_probability_max}</td>
-                        <td align="right">{parseFloat(day.wind_speed_10m_max * 1000 / 3600).toFixed(1)}</td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
-    );
-} else {
-    return <div>Ei leitud prognoosi valitud asukohale.</div>;
-}
-}
+            ))}
+        </tbody>
+    </table>
+
+};
 
 export default WeatherForecast;
 
